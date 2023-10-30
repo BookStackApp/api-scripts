46e79690d0228a0d44aafb3457363a2cf46cdd2d
Skiddybison5924 <101336067+chris-devel0per@users.noreply.github.com>
2023-10-30 18:29:50 +0100

    Delete flutter-example directory

--- flutter-example/android/app/src/main/kotlin/com/bookstackapp/bookstackapi/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.bookstackapp.bookstackapi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter-example/lib/controller/server_info.dart
+++
@@ -1,9 +0,0 @@
-const String urlFull= 'http://example.com';
-
-const String tokenId = 'token';
-const String secret = 'secret';
-String token = 'Token $tokenId:$secret';
-
-final headers = {
-  'Authorization': token,
-};
